565dba1f8847891ee5c152bdecdca8b7ee06bf43
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-01 18:13:50 +0100

    task 2 first solution

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// не по тз
+const render = (array) => {
+
+  array.forEach(itemText => {
+    const newListItem = document.createElement("li");
+    newListItem.textContent = itemText;
+    newListItem.classList.add('item');
+    const list = document.querySelector('#ingredients');
+    list.append(newListItem);
+  });
+}
+
+render(ingredients);
+ 
