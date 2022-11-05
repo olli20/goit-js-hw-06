c95f3625eadedc0e5f485b5341b485049e51518e
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-05 19:33:29 +0100

    task 9 first solution

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  magicBtn: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+  colorHex: document.querySelector('.color'),
+}
+
+refs.magicBtn.addEventListener('click', onClickBtnChangeBackground);
+
+function onClickBtnChangeBackground() {
+  const randomColor = getRandomHexColor();
+  refs.body.style.backgroundColor = `${randomColor}`;
+  refs.colorHex.textContent = randomColor;
+}
