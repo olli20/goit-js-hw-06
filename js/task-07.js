7a7f7eb8cd4fc7924b6e08be68c667c9bd124afd
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-05 19:05:07 +0100

    task 8 first solution

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,4 @@
 const fontSizeInput = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
 
-fontSizeInput.addEventListener('input', (event) => {
-    text.style.fontSize = `${event.currentTarget.value}px`;
-});
+fontSizeInput.addEventListener('input', (event) => text.style.fontSize = `${event.currentTarget.value}px`);
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault(); 
+
+    const inputData = {};
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+   
+    if (email.value === "" || password.value === "") {
+        alert("Please fill in all the fields!");
+    } else {
+        inputData.email = email.value;
+        inputData.password = password.value;
+        console.log(inputData);
+
+        event.currentTarget.reset();
+    }
+}
