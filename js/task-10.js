cb7b1401aac542c2fd9f32aac175fa382d1d3924
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-06 12:22:09 +0100

    task 10 second solution

--- js/task-10.js
+++ js/task-10.js
@@ -10,6 +10,7 @@ const refs = {
 }
 
 function createBoxes(amount) {
+    amount = refs.userInput.value;
     const bunchOfBoxes = [];
 
     let initialWidth = 30;
@@ -22,14 +23,45 @@ function createBoxes(amount) {
     }
     refs.boxes.append(...bunchOfBoxes);
 }
-
-const destroyBoxes = () => {
+  
+function destroyBoxes() {
   refs.boxes.innerHTML = '';
 }
 
-refs.createBtn.addEventListener('click', createBoxes);
 refs.destroyBtn.addEventListener('click', destroyBoxes);
+refs.createBtn.addEventListener('click', createBoxes);
 
 
 
 
+// // first solution
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// const refs = {
+//   boxes: document.querySelector('#boxes'),
+//   userInput: document.querySelector('#controls input'),
+//   createBtn: document.querySelector('button[data-create]'),
+//   destroyBtn: document.querySelector('button[data-destroy]'),
+// }
+
+// refs.createBtn.addEventListener('click', () => {
+//     const currentInput = refs.userInput.value;
+//     const bunchOfBoxes = [];
+
+//     let initialWidth = 30;
+
+//     for (let i = 0; i < currentInput; i += 1) {
+//       console.log(initialWidth);
+//       const oneBox = document.createElement('div');
+//       oneBox.style = `width: ${initialWidth}px; height: ${initialWidth}px; background-color: ${getRandomHexColor()};`;
+//       bunchOfBoxes.push(oneBox);
+//       initialWidth += 10;
+//     }
+//     refs.boxes.append(...bunchOfBoxes);
+//   });
+
+// refs.destroyBtn.addEventListener('click', () => {
+//   refs.boxes.innerHTML = '';
+// });
