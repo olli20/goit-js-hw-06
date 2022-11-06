dac7ea020a106ca77241aa50edb308cd9edbc930
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-06 13:08:05 +0100

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const textInput = document.querySelector('#validation-input');
 
 textInput.addEventListener('blur', () => {
 
-    if (textInput.value.length == textInput.getAttribute('data-length')) {
+    if (textInput.value.trim().length == textInput.getAttribute('data-length')) {
         if (textInput.classList.contains('invalid')) {
             textInput.classList.replace('invalid', 'valid');
         } else if (!textInput.classList.contains('valid')) {
