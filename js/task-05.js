deffd61305d4411d745a5daaf6b66f4cd7c83e7f
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-06 13:05:22 +0100

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const userName = document.querySelector('#name-output');
 const textInput = document.querySelector('#name-input');
 
 textInput.addEventListener("input", (event) => {
-    if (event.currentTarget.value != '') {
+    if (event.currentTarget.value.trim() != '') {
         userName.textContent = event.currentTarget.value;
     } else { userName.textContent = 'Anonymous' }
 });
